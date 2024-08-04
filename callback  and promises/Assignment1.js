d70c05656448b56f367a32faeb1bff07a46233ba
DeveloperSoumyadeep2024 <soumyadeeptaladhi@gmail.com>
2024-08-04 20:20:58 +0530

    Add files via upload

---
+++ callback  and promises/Assignment1.js
@@ -0,0 +1,10 @@
+function sumAsync(a,b, callback){
+    setTimeout(()=>{
+        const result =a+b;
+        callback(result);
+
+    },1000);
+}
+sumAsync(3,7,(result)=>{
+    console.log(result);//output:10
+});
---
+++ callback  and promises/Assignment5.js
@@ -0,0 +1,9 @@
+function multiplyWithCallback(numbers,callback){
+    const multipliedArray=numbers.map((num)=>num*2);
+    callback(multipliedArray);
+}
+
+const inputArray=[1,2,3,4];
+multiplyWithCallback(inputArray,(result)=>{
+    console.log(result);
+});
